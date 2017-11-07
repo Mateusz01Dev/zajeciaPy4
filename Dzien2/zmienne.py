dfda9fec22e9a09fb9c8aafd71ac11056d62bc56
Mateusz01Dev <mateuszrogala8@gmail.com>
2017-11-07 20:32:12 +0100

    pliki dzien 2

---
+++ Dzien2/zmienne.py
@@ -0,0 +1,33 @@
+imie = "Joanna"
+
+nazwisko = "Kowalska"
+
+wiek = 34
+
+print(imie + ' ' + nazwisko + 'ma' + str(wiek) + 'lata.')
+
+print(f"{imie} {nazwisko} ma {wiek} lata.")
+
+
+
+print(imie[3:5])
+
+print (3 + 7)
+
+print (3 == 5)
+
+print('Dzień dobry')
+
+aktRok = 2017
+
+print(imie.lower())
+
+print(imie.count('a'))
+
+pythonRok= 1989
+
+wiekPythona = aktRok - pythonRok
+
+imie = input ('Jak się nazywasz?')
+
+wiek = input('Ile masz lat?')
---
+++ Dzien2/zmienne2.py
@@ -0,0 +1,26 @@
+#imie = input('Jak się nazywasz?')
+#wiek = int(input('Ile masz lat?'))
+
+#print ("Witaj", imie)
+#print ("Jestem Python, mam", wiekPythona, "lat)
+
+#if wiek > wiekPythona:
+#print('Jesteś starszy ode mnie')
+
+zdanie = "Ala ma kota, kot ma Ale."
+zdanie2 = " Ale ja wole wolę psy!"
+
+print (zdanie, end='....... ')
+print (zdanie2)
+
+print(zdanie + zdanie2)
+print(zdanie, zdanie2)
+
+
+#moj_plik = "C:\teamwork\nowy_folder\skrypt.py"
+#moj_plik2 = "C:\\teamwork\\nowy_folder\skrypt.py"
+#moj_plik3 = r"C:\\teamwork\\nowy_folder\skrypt.py"
+
+#print(moj_plik)
+#print(moj_plik2)
+#print(moj_plik3)
